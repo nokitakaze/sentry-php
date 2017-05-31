d1d27a697a45fad390009c2f89236cd0bc40c977
Nokita Kaze <admin@kanaria.ru>
2017-06-01 02:13:00 +0300

    Refactoring for async calls Curl (exec): CA-cert Coverage if events really sending Monolog fix for PHP 7

---
+++ tests/bin/httpserver.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+<?php
+echo "init...\n";
+require_once __DIR__.'/../../vendor/autoload.php';
+$cli_param = getopt('', ['config:',]);
+
+/** @var \NokitaKaze\TestHTTPServer\ServerSettings $options */
+$options = unserialize(file_get_contents($cli_param['config']));
+
+$options->onRequest = function ($server, $connect) {
+    /** @var \NokitaKaze\TestHTTPServer\Server $server */
+    /** @var \NokitaKaze\TestHTTPServer\ClientDatum $connect */
+    $output_filename = $server->get_option('output_filename');
+
+    $connect->server = null;
+    $saved = file_put_contents(
+        $output_filename, serialize(
+        [
+            'connection' => $connect,
+        ]
+    ), LOCK_EX
+    );
+    if ($saved === false) {
+        $server->answer($connect, 500, 'OK', '');
+        $server->close_connection($connect);
+        echo "can not save content\n";
+        exit(1);
+    }
+
+    $server->answer($connect, 200, 'OK', json_encode(['event' => uniqid()]));
+    $server->close_connection($connect);
+    echo "done\n";
+    exit(0);
+};
+$server = new \NokitaKaze\TestHTTPServer\Server($options);
+
+$server->init_listening();
+echo "listen...\n";
+$server->listen(time() + 60);
+
+echo "No connection\n";
+exit(1);
+?>
